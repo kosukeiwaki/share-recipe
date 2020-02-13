fa930a8b7f650c171e52e7207ba1b84ca2664eea
kosukeiwaki <k544274@gmail.com>
2020-02-13 14:23:37 +0900

    make_device
    deviceのインストールの直前まで

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module ShareRecipe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    protect_from_forgery with: :exception
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
