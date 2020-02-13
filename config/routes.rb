79c553f3048925b9128841c223cf193e41ffdc7b
kosukeiwaki <k544274@gmail.com>
2020-02-13 16:03:11 +0900

    make_sessionview

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'recipe/index'
+  root "recipe#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root "recipe#index"
 
 end
