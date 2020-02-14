52715456b05e1664a77f4e8b6b0cf05707718ffb
kosukeiwaki <k544274@gmail.com>
2020-02-14 16:29:56 +0900

    make_detail

--- app/controllers/recipe_controller.rb
+++
@@ -1,6 +0,0 @@
-class RecipeController < ApplicationController
-  
-  def index
-    @recipes = Recipe.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "recipe#index"
-  resources :recipes, only: [:index, :new]
+  root "recipes#index"
+  resources :recipes, only: [:index, :new, :create, :show]
   resources :users, only: [:edit, :update]
 
 end
