7fb4cab67a393faa85cf4c84ac8e8428e54a044d
kosukeiwaki <k544274@gmail.com>
2020-02-14 10:58:55 +0900

    change_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "recipe#index"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "recipe#index"
+  resources :users, only: [:edit, :update]
 
 end
