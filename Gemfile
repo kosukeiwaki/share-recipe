f6c7b4e8bf19db9cd3e8e0b249fcaab7eff87a89
kosukeiwaki <k544274@gmail.com>
2020-03-15 10:19:46 +0900

    fix

--- Gemfile
+++ Gemfile
@@ -38,7 +38,6 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
-  gem 'devise'
 end
 
 group :development do
@@ -56,6 +55,7 @@ gem 'haml-rails'
 gem 'font-awesome-sass'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'devise'
 
 group :production do
   gem 'unicorn', '5.4.1'
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20200214085957) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "text"
+    t.text     "image",      limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
   create_table "recipes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "title"
