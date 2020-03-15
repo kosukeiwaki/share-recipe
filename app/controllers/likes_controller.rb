cf8d90c116096ba1c03eb2be7521dcc342a7481f
kosukeiwaki <k544274@gmail.com>
2020-03-15 17:21:47 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,5 @@
 class LikesController < ApplicationController
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
     
     def create
       @like = current_user.likes.create(recipe_id: params[:recipe_id])
