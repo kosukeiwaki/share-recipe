8202e25a411c17512ab014e9668ec6a318c5da4d
kosukeiwaki <k544274@gmail.com>
2020-03-19 21:16:27 +0900

    fix_Google_API

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,58 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  def index
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def new
+    @user = User.new
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def create
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+    if params[:sns_auth] == 'true'
+      pass = Devise.friendly_token
+      params[:user][:password] = pass
+      params[:user][:password_confirmation] = pass
+    end
+  
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+    end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
 
-  # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+    def create_address
+      @user = User.new(session["devise.regist_data"]["user"])
+      @address = Address.new(address_params)
+      unless @address.valid?
+        flash.now[:alert] = @address.errors.full_messages
+        render :new_address and return
+      end
+      @user.build_address(@address.attributes)
+      @user.save
+      sign_in(:user, @user)
+      redirect_to root_path
+    end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+ 
+  protected
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def address_params
+    params.require(:address).permit(:zip,:prefecture, :city, :block, :building, :phone_number,
+    :lastname, :firstname, :lastname_kana, :firstname_kana)
+  end
+
 end
